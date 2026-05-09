9db1a5ab019faa38692f1cc6a4f594c1ff27367c
CameliaPopa96 <127297435+CameliaPopa96@users.noreply.github.com>
2026-05-09 20:50:19 +0300

    Update ap.js

--- ap.js
+++ ap.js
@@ -1,1 +1,24 @@
+async function loadImpact(){
 
+  const res = await fetch(`${API_URL}/impact`);
+  const data = await res.json();
+
+  document.getElementById("impact").innerHTML = `
+    Pacienți ajutați: ${data.patients}<br>
+    Donații: ${data.donations} RON
+  `;
+}
+
+loadImpact();
+
+async function donate(amount){
+
+  const res = await fetch(`${API_URL}/donations/checkout`, {
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify({amount})
+  });
+
+  const data = await res.json();
+  window.location.href = data.url;
+}
